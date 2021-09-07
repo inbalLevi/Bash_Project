fe86279803e2fc2bc1ee758d6d26eba8fdf71672
inbal levi <inbalevi2014@gmail.com>
2021-09-07 22:13:22 +0300

    changes were done

--- password-validator.sh
+++ password-validator.sh
@@ -4,34 +4,31 @@
 # colors and OK status.
 # Run the script: ./password-validator.sh '[password]'
 
+#the first input argument 
 PASS=$1
 REASON=""
+ISGOOD=0
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
-OK=0
 
 # Check if string is less than 10 characters
+if [ ${#PASS} -ge 10 ]; then ((ISGOOD++))
+else REASON+="your password does not include 10 or more characters\n"; fi
 
-if [ ${#PASS} -ge 10 ]; then ((OK++))
-else REASON+="password does not include 10 or more characters\n"; fi
-
-# Check if string include uppercase letters
-
-if [[ $PASS =~ [A-Z] ]]; then ((OK++))   
-else REASON+="password does not include uppercase letters\n"; fi
 
 # Check if string include lowercase letters
+if [[ $PASS =~ [a-z] ]]; then ((ISGOOD++))
+else REASON+="your password does not include lowercase letters\n"; fi
 
-if [[ $PASS =~ [a-z] ]]; then ((OK++))
-else REASON+="password does not include lowercase letters\n"; fi
+# Check if string include uppercase letters
+if [[ $PASS =~ [A-Z] ]]; then ((ISGOOD++))   
+else REASON+="your password does not include uppercase letters\n"; fi
 
 # Check if string include digits
+if [[ $PASS =~ [0-9] ]]; then ((ISGOOD++))
+else REASON+="your password does not include digits\n"; fi
 
-if [[ $PASS =~ [0-9] ]]; then ((OK++))
-else REASON+="password does not include digits\n"; fi
-
-# Check OK status, if OK doesn't equal to 4, it means that password isn't good enough 
+# Check ISGOOD status, if ISGOOD doesn't equal to 4, it means that password isn't good enough 
 # since it doesn't meet the conditions above
-
-if [ $OK == 4 ]; then echo "${GREEN}Your password is awesome!"; exit 0
+if [ $ISGOOD == 4 ]; then echo "${GREEN}Your password is awesome!"; exit 0
 else echo -e "${RED}Weak Password\n$REASON"; fi
